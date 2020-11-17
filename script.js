38b551870ec7ff7d31cce2e288f4f57ea05bc79f
Jess Parnow <jessparnow@gmail.com>
2020-11-16 18:53:31 -0600

    adding updated code

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-
 let m = moment();
 
-console.log(m.format("L"));
+// console.log(m.format("L"));
 //add in one day to pull for the 5 day forecast
 let dateDisplay = $("#currentDay");
 
@@ -13,109 +12,115 @@ function currentweather(cityName) {
   let queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
-    "&units=imperial&appid=";
+    "&units=imperial&appid=b4b407e1c227fb03804663cfcb368554";
 
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
     // console.log(response);
+    let icon = $('#weather-icon');
+    icon.html('<img>' + response.weather[0].icon + '</>')
     let temp = $("#currentDayTemp");
     temp.html(
-      '<p> Temperature: ' + response.main.temp +
+      '<p class="weatherinfo"> Temperature: ' + response.main.temp +
       '&#176; F </p>');
 
     let humidity = $("#currentDayHumidity");
     humidity.html(
-     '<p> Humidity: ' + response.main.humidity +
+     '<p class="weatherinfo"> Humidity: ' + response.main.humidity +
      '&#37; </p>');
 
     let windSpeed = $("#currentDayWind");
     windSpeed.html(
-      '<p> Wind Speed: ' + response.wind.speed +
-      '<p>');
+      '<p class="weatherinfo"> Wind Speed: ' + response.wind.speed +
+      'mph</p>');
   });
+
 }
 
 function fiveDay(cityName) {
   let queryURL =
     "https://api.openweathermap.org/data/2.5/forecast?q=" +
     cityName +
-    "&units=imperial&appid=";
+    "&units=imperial&appid=b4b407e1c227fb03804663cfcb368554";
   $.ajax({
     url: queryURL,
     method: "GET",
   }).then(function (response) {
-    console.log(response);
+    // console.log(response);
 
     let dayOneTemp = $("#day-one-temp");
     dayOneTemp.html(
-      '<p> Temp: ' +
+      '<p class="fiveDayInfo"> Temp: ' +
         response.list[3].main.temp +
         "&#176; F </p>" 
     );
     let dayTwoTemp = $("#day-two-temp");
     dayTwoTemp.html(
-      '<p> Temp: ' +
+      '<p class="fiveDayInfo"> Temp: ' +
         response.list[11].main.temp +
         "&#176; F </p>" 
     );
     let dayThreeTemp = $("#day-three-temp");
     dayThreeTemp.html(
-      '<p> Temp: ' +
+      '<p class="fiveDayInfo"> Temp: ' +
         response.list[19].main.temp +
         "&#176; F </p>" 
     );
     let dayFourTemp = $("#day-four-temp");
     dayFourTemp.html(
-      '<p> Temp: ' +
+      '<p class="fiveDayInfo"> Temp: ' +
         response.list[27].main.temp +
         "&#176; F </p>" 
     );
     let dayFiveTemp = $("#day-five-temp");
     dayFiveTemp.html(
-      '<p> Temp: ' +
+      '<p class="fiveDayInfo"> Temp: ' +
         response.list[35].main.temp +
         "&#176; F </p>" 
     );
     let dayOneHumidity = $("#day-one-humidity");
     dayOneHumidity.html(
-      '<p> Humidity: ' +
+      '<p class="fiveDayInfo"> Humidity: ' +
         response.list[3].main.humidity +
         "&#37; </p>" 
     );
     let dayTwoHumidity = $("#day-two-humidity");
     dayTwoHumidity.html(
-      '<p> Humidity: ' +
+      '<p class="fiveDayInfo"> Humidity: ' +
         response.list[11].main.humidity +
         "&#37; </p>" 
     );
     let dayThreeHumidity = $("#day-three-humidity");
     dayThreeHumidity.html(
-      '<p> Humidity: ' +
+      '<p class="fiveDayInfo"> Humidity: ' +
         response.list[19].main.humidity +
         "&#37; </p>" 
     );
     let dayFourHumidity = $("#day-four-humidity");
     dayFourHumidity.html(
-      '<p> Humidity: ' +
+      '<p class="fiveDayInfo"> Humidity: ' +
         response.list[27].main.humidity +
         "&#37; </p>" 
     );
     let dayFiveHumidity = $("#day-five-humidity");
     dayFiveHumidity.html(
-      '<p> Humidity: ' +
+      '<p class="fiveDayInfo"> Humidity: ' +
         response.list[35].main.humidity +
         "&#37; </p>" 
     );
   });
 }
+let citySearchInput = $("#search-input").val();
+let lastSearchedCities = $("ul");
+    let cityListItems = $("<li>");
+    let cities = [];
 
-
-$("#search-button").on("click", function (event) {
-  let citySearchInput = $("#search-input").val();
-  event.preventDefault();
-  console.log(citySearchInput);
+$("#search-button").on("click", function () {
+  
+  // event.preventDefault();
+  // console.log(citySearchInput);
   //pulls the weather from the api
   currentweather(citySearchInput);
   fiveDay(citySearchInput);
@@ -129,8 +134,7 @@ $("#search-button").on("click", function (event) {
     // find ul
     //create the list items
     // attach li to ul
-    let lastSearchedCities = $("ul");
-    let cityListItems = $("<li>");
+    
     cityListItems.text(citySearchInput)
     lastSearchedCities.append(cityListItems);
 
@@ -141,9 +145,15 @@ $("#search-button").on("click", function (event) {
     //add city Name into the header element
     let currentCityHeader = $("#city-name");
     currentCityHeader.text(citySearchInput);
+    setCityName();
+  //   for (let i = 0; i < localStorage.length; i++){
+  //     //selecting the id and placing into the display key() represents the key in the object 
+  //      $('<li>' + localStorage.key(i)).val(localStorage.getItem(localStorage.key(i)));
+  //     console.log(localStorage.key(i))
+  // }
   });
 
-  
+  console.log(localStorage)
 });
 
 function fiveDayDisplay(){
@@ -161,4 +171,12 @@ dayFourDisplay.text(m.add(1, 'days').format('L'));
 
 let dayFiveDisplay = $("#day-five");
 dayFiveDisplay.text(m.add(1, 'days').format('L'));
+}
+function setCityName(){
+  localStorage.setItem('city name', citySearchInput);
+}
+localStorage.getItem(cityListItems);
+
+if (cityListItems === !null){
+  cities = cityListItems;
 }
